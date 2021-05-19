77e9a81cb660653654c330c8d5a3b89931956e71
Mgainullin <m.gainullin@proton-electrotex.com>
2021-05-19 11:27:18 +0300

    Конфигурация пинов управления диапазонами скоростей, ПДП и АЦП, таймер 7

--- Firmware/Source/Controller/InitConfig.c
+++ Firmware/Source/Controller/InitConfig.c
@@ -7,6 +7,7 @@
 #include "DataTable.h"
 #include "DeviceObjectDictionary.h"
 #include "BCCIxParams.h"
+#include "Measurement.h"
 
 // Functions
 //
@@ -19,7 +20,7 @@ Boolean INITCFG_ConfigSystemClock()
 void INITCFG_ConfigEI()
 {
 	// Sync
-	EXTI_Config(EXTI_PB, EXTI_0, FALL_TRIG, 0);
+	EXTI_Config(EXTI_PB, EXTI_0, BOTH_TRIG, 0);
 	EXTI_EnableInterrupt(EXTI0_IRQn, 0, true);
 }
 //------------------------------------------------------------------------------
@@ -39,27 +40,32 @@ void INITCFG_ConfigIO()
 
 	// Выходы
 	GPIO_InitPushPullOutput(GPIO_I_LIM);
-	GPIO_SetState(GPIO_I_LIM, false);
-	GPIO_InitPushPullOutput (GPIO_BRAKE_PWM);
-	GPIO_SetState(GPIO_BRAKE_PWM, false);
+	GPIO_SetState(GPIO_I_LIM, true);
 	GPIO_InitPushPullOutput (GPIO_LED1);
 	GPIO_SetState(GPIO_LED1, false);
 	GPIO_InitPushPullOutput (GPIO_LED2);
 	GPIO_SetState(GPIO_LED2, false);
+	GPIO_InitPushPullOutput(GPIO_LOW_RATE_CHNNL);
+	GPIO_SetState(GPIO_LOW_RATE_CHNNL, false);
+	GPIO_InitPushPullOutput (GPIO_MID_RATE_CHNNL);
+	GPIO_SetState(GPIO_MID_RATE_CHNNL, false);
+	GPIO_InitPushPullOutput (GPIO_HIGH_RATE_CHNNL);
+	GPIO_SetState(GPIO_HIGH_RATE_CHNNL, false);
 
 	// Альтернативные функции
 	GPIO_InitAltFunction(GPIO_ALT_UART_RX, AltFn_7);
 	GPIO_InitAltFunction(GPIO_ALT_UART_TX, AltFn_7);
-	GPIO_InitAltFunction(GPIO_ALT_PWM, AltFn_9);
+	GPIO_InitAltFunction(GPIO_ALT_PWM_FB, AltFn_9);
+	GPIO_InitAltFunction(GPIO_ALT_PWM_BRAKE, AltFn_1);
 }
 //------------------------------------------------------------------------------
 
 void INITCFG_ConfigUART()
 {
 	USART_Init(USART1, SYSCLK, USART_BAUDRATE);
-	USART_Recieve_Interupt(USART1, 0, true);
+	USART_Recieve_Interupt(USART1, 2, true);
 }
-//------------------------------------------------
+//------------------------------------------------------------------------------
 
 void INITCFG_ConfigADC()
 {
@@ -67,14 +73,28 @@ void INITCFG_ConfigADC()
 
 	ADC_Calibration(ADC1);
 	ADC_SoftTrigConfig(ADC1);
-	ADC_Enable(ADC1);
+	ADC_ChannelSeqReset(ADC1);
 
-	//ADC_Calibration(ADC1);
-	//ADC_TrigConfig(ADC1, ADC12_TIM6_TRGO, RISE);
-	//ADC_ChannelSeqReset(ADC1);
-	//ADC_ChannelSet_Sequence(ADC1, ADC_CH_V_CAP, 1);
-	//ADC_ChannelSeqLen(ADC1, 1);
-	//ADC_Enable(ADC1);
+	for (uint8_t i = 1; i <= ADC_DMA_BUFF_SIZE; ++i)
+		ADC_ChannelSet_Sequence(ADC1, ADC1_V_BAT_CHANNEL, i);
+
+	ADC_ChannelSeqLen(ADC1, ADC_DMA_BUFF_SIZE);
+	ADC_DMAConfig(ADC1);
+	ADC_Enable(ADC1);
+}
+//------------------------------------------------------------------------------
+
+void INITCFG_ConfigDMA()
+{
+	DMA_Clk_Enable(DMA1_ClkEN);
+
+	// DMA для АЦП напряжения батареи
+
+	DMA_Reset(DMA_ADC_V_BAT_CHANNEL);
+	DMAChannelX_Config(DMA_ADC_V_BAT_CHANNEL, DMA_MEM2MEM_DIS, DMA_LvlPriority_LOW, DMA_MSIZE_16BIT, DMA_PSIZE_16BIT,
+							DMA_MINC_EN, DMA_PINC_DIS, DMA_CIRCMODE_EN, DMA_READ_FROM_PERIPH);
+	DMAChannelX_DataConfig(DMA_ADC_V_BAT_CHANNEL, (uint32_t)(&MEASURE_ADC_BatteryVoltageRaw[0]), (uint32_t)(&ADC1->DR), ADC_DMA_BUFF_SIZE);
+	DMA_ChannelEnable(DMA_ADC_V_BAT_CHANNEL, true);
 }
 //------------------------------------------------------------------------------
 
@@ -82,7 +102,7 @@ void INITCFG_ConfigTimer3()
 {
 	TIM_Clock_En(TIM_3);
 	TIM_Config(TIM3, SYSCLK, TIMER3_uS);
-	TIM_Interupt(TIM3, 1, true);
+	TIM_Interupt(TIM3, 3, true);
 	TIM_Start(TIM3);
 }
 //------------------------------------------------------------------------------
@@ -91,15 +111,23 @@ void INITCFG_ConfigTimer6()
 {
 	TIM_Clock_En(TIM_6);
 	TIM_Config(TIM6, SYSCLK, TIMER6_uS);
-	TIM_DMA(TIM6, DMAEN);
 	TIM_MasterMode(TIM6, MMS_UPDATE);
+	TIM_Start(TIM6);
 }
-//------------------------------------------------
+//------------------------------------------------------------------------------
+
+void INITCFG_ConfigTimer7()
+{
+	TIM_Clock_En(TIM_7);
+	TIM_Config(TIM7, SYSCLK, TIMER7_uS);
+	TIM_Interupt(TIM7, 1, true);
+}
+//------------------------------------------------------------------------------
 
 void INITCFG_ConfigWatchDog()
 {
 	IWDG_Config();
-	IWDG_ConfigureFastUpdate();
+	IWDG_ConfigureSlowUpdate();
 }
 //------------------------------------------------------------------------------
 
@@ -108,11 +136,13 @@ void INITCFG_ConfigPWM()
 	//PA2 - Flyback PWM
 	TIM_Clock_En(TIM_15);
 	TIM15_16_17_PWM_CH1_Config(TIM15, SYSCLK, TIMER15_uS);
+	TIM15->CCER &= PWM_OUT_LOW_POL;
 	TIM_Start(TIM15);
 
 	//PA6 - Brake PWM
 	TIM_Clock_En(TIM_16);
 	TIM15_16_17_PWM_CH1_Config(TIM16, SYSCLK, TIMER16_uS);
+	TIM16->CCER &= PWM_OUT_LOW_POL;
 	TIM_Start(TIM16);
 }
 //------------------------------------------------------------------------------
@@ -121,8 +151,7 @@ void INITCFG_ConfigDAC()
 {
 	DACx_Clk_Enable(DAC_1_ClkEN);
 	DACx_Reset();
-	//DAC_TriggerConfigCh1(DAC1, TRIG1_TIMER6, TRIG1_ENABLE);
-	DAC_TriggerConfigCh1(DAC1, TRIG1_SOFTWARE, TRIG1_ENABLE);
+	DAC_TriggerConfigCh1(DAC1, TRIG1_TIMER6, TRIG1_ENABLE);
 	DAC_BufferCh1(DAC1, false);
 	DAC_EnableCh1(DAC1);
 }
