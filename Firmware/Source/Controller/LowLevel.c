7b2299d05edc7d79a9896d78929fbc37a786d50a
Mgainullin <m.gainullin@proton-electrotex.com>
2021-05-19 11:23:58 +0300

    Функции управления пинами контроллера (ограничение тока, переключение скоростей)

--- Firmware/Source/Controller/LowLevel.c
+++ Firmware/Source/Controller/LowLevel.c
@@ -17,3 +17,28 @@ void LL_ExternalLED(bool State)
 	GPIO_SetState(GPIO_LED1, State);
 }
 //-----------------------------
+
+// I_LIM
+void LL_SW_I_LIM(bool State)
+{
+	GPIO_SetState(GPIO_I_LIM, State);
+}
+//-----------------------------
+
+void LL_SW_LOW_RATE_CHNNL(bool State)
+{
+	GPIO_SetState(GPIO_LOW_RATE_CHNNL, State);
+}
+//-----------------------------
+
+void LL_SW_MID_RATE_CHNNL(bool State)
+{
+	GPIO_SetState(GPIO_MID_RATE_CHNNL, State);
+}
+//-----------------------------
+
+void LL_SW_HIGH_RATE_CHNNL(bool State)
+{
+	GPIO_SetState(GPIO_HIGH_RATE_CHNNL, State);
+}
+//-----------------------------
--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -9,5 +9,9 @@
 //
 void LL_ToggleLed();
 void LL_ExternalLED(bool State);
+void LL_SW_I_LIM(bool State);
+void LL_SW_LOW_RATE_CHNNL(bool State);
+void LL_SW_MID_RATE_CHNNL(bool State);
+void LL_SW_HIGH_RATE_CHNNL(bool State);
 
 #endif //__LOWLEVEL_H
